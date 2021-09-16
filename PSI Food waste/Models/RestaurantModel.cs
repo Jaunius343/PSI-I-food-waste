8495bf17191302e89b6aa101514cd7d26d6ce6d5
GamingGolem <niurkasadainius@gmail.com>
2021-09-16 12:46:38 +0300

    Add a Restaurant data collector

--- PSI Food waste/Models/RestaurantModel.cs
+++ PSI Food waste/Models/RestaurantModel.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace PSI_Food_waste.Models
 {
-    public class LocationModel
+    public class RestaurantModel
     {
-        public string City { get; set; }
-        public string Street { get; set; }
+        public string RestaurantName {  get; set;}
+        public string RestaurantAdress {  get; set;}
+        public string RestaurantWorkerID {  get; set;}
 
     }
 }
