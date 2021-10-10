eea159c6c64f5b38f92988a20f2d5c6151ab0fbf
Jaunius343 <73691989+Jaunius343@users.noreply.github.com>
2021-10-10 18:56:55 +0300

    implement group join

---
+++ PSI Food waste/Pages/Forms/groupJoinTest.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PSI_Food_waste.Models;
+using PSI_Food_waste.Services;
+
+namespace PSI_Food_waste.Pages.Forms
+{
+    public class groupJoinTestModel : PageModel
+    {
+        public List<Product> products { get; set; }
+        public List<Restaurant> restaurants { get; set; }
+
+        public void OnGet()
+        {
+            restaurants = RestaurantServices.GetAll();
+            products = ProductService.GetAll();
+        }
+    }
+}
