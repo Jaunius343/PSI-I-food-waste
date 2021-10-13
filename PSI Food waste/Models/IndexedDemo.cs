aa848434bd2d3925f1824bfb902f994a4c9a4125
Jaunius343 <73691989+Jaunius343@users.noreply.github.com>
2021-10-13 10:50:57 +0300

    add indexed property

---
+++ PSI Food waste/Models/IndexedDemo.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSI_Food_waste.Models
+{
+    public class IndexedDemo<T>
+    {
+        private T[] array = new T[100];
+
+        public T this[int i]
+        {
+            get => array[i];
+            set => array[i] = value;
+        }
+    }
+}
